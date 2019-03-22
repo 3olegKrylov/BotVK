6f896408f182cd1a7af5447766fa20d7e950fb03
3olegKrylov <43906382+3olegKrylov@users.noreply.github.com>
2019-03-22 21:37:28 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/dimonchik0036/vk-api"
+	"log"
+)
+
+func main() {
+	//client, err := vkapi.NewClientFromLogin("89219850412", "prioritet", vkapi.ScopeMessages)
+	client, err := vkapi.NewClientFromToken("f8195384c2cd1b11c19a0d99388bd43e63db3e771bb32223f2c11047337f343726b7777e6b061901b6292")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client.Log(true)
+
+	if err := client.InitLongPoll(0, 2); err != nil {
+		log.Panic(err)
+	}
+
+	updates, _, err := client.GetLPUpdatesChan(100, vkapi.LPConfig{25, vkapi.LPModeAttachments})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil || !update.IsNewMessage() || update.Message.Outbox(){
+			continue
+		}
+
+		log.Printf("%s", update.Message.String())
+		if update.Message.Text == "/info" {
+			client.SendMessage(vkapi.NewMessage(vkapi.NewDstFromUserID(update.Message.FromID), "Привет, с вами на линии бот Олега, если хотите,(информцию обо мне /info),спросить как дела(/какдела) чтобы я поздоровался (/privet) и  (/что)"))
+		}
+
+		if update.Message.Text == "/какдела" {
+			client.SendMessage(vkapi.NewMessage(vkapi.NewDstFromUserID(update.Message.FromID), "хорошо, не спрашивай больше меня ни о чём"))
+		}
+
+		if update.Message.Text == "/privet" {
+			client.SendMessage(vkapi.NewMessage(vkapi.NewDstFromUserID(update.Message.FromID), "Hello!"))
+		}
+
+		if update.Message.Text == "/dania" {
+			client.SendMessage(vkapi.NewMessage(vkapi.NewDstFromUserID(update.Message.FromID), "Даня лох..."))
+		}
+		if update.Message.Text == "/vova" {
+			client.SendMessage(vkapi.NewMessage(vkapi.NewDstFromUserID(update.Message.FromID), "О святой господин Геюг"))
+		}
+
+		if update.Message.Text == "/что" {
+			client.SendMessage(vkapi.NewMessage(vkapi.NewDstFromUserID(update.Message.FromID), "могу всё что угодно"))
+		}
+
+	}
+}
